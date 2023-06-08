64373e09b5d0673cfb2d8ca40a3b38c7bc052146
zabaidulbd <zislam5622@gmail.com>
2023-06-08 12:46:54 +0600

    add users

--- index.js
+++ index.js
@@ -28,12 +28,33 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const usersCollection = client.db("twelveDb").collection("users");
+
+
+        // users api
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result)
+
+        });
+
+
+
+
+
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
