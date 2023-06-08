8471ae3bf646899f7fbd1d9d22855dbe32915038
zabaidulbd <zislam5622@gmail.com>
2023-06-08 20:08:44 +0600

    add instructor

--- index.js
+++ index.js
@@ -97,6 +97,20 @@ async function run() {
         })
 
 
+        app.get('/users/instructor/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+
+            if (req.decoded.email !== email) {
+                res.send({ instructor: false })
+            }
+
+            const query = { email: email }
+            const user = await usersCollection.findOne(query);
+            const result = { instructor: user?.role === 'instructor' }
+            res.send(result);
+        })
+
+
 
 
 
