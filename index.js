c760736bbdfb462197c3a32a86bbca8680898d9c
zabaidulbd <zislam5622@gmail.com>
2023-06-08 13:15:01 +0600

    add social

--- index.js
+++ index.js
@@ -36,6 +36,11 @@ async function run() {
 
         app.post('/users', async (req, res) => {
             const user = req.body;
+            const query = { email: user.email }
+            const existUser = await usersCollection.findOne(query)
+            if (existUser) {
+                return res.send({ message: 'user already exists' })
+            }
             const result = await usersCollection.insertOne(user);
             res.send(result)
 
